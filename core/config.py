46c727b94d16c7cda5e57455fcbb9c5fc27a0960
Rustam.Home <ka.po.po@yandex.ru>
2024-10-23 23:16:27 +0500

    Рефакторинг

--- core/config.py
+++ core/config.py
@@ -5,15 +5,15 @@ class Settings:
     PROJECT_NAME: str = "FastMSG"
     PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "test1234")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5433)
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "fast_msg")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "test")
-    JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
     JWT_EXPIRATION_TIME = timedelta(minutes=480)
 
 
