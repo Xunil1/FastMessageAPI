9ae850e9fa0ede72ed2c1d7f883bbf64f1676376
Rustam.Home <ka.po.po@yandex.ru>
2024-10-23 22:27:58 +0500

    Добавление в модели пользователя поля telegram_id

--- db/models/user.py
+++ db/models/user.py
@@ -1,5 +1,5 @@
 from db.base_class import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -8,6 +8,7 @@ class User(Base):
     username = Column(String, nullable=False, unique=True, index=True)      # Никнейм
     hash_password = Column(String, nullable=False)                          # Хэш пароля
     token = Column(String, default=None, nullable=True)
+    telegram_id = Column(Integer, default=None, nullable=True)
 
     chats = relationship('Chat', secondary='userchat', back_populates='users', lazy="selectin")
     sent_messages = relationship('Message', back_populates='sender', lazy="selectin")
