57108eb1cac3334a77b7d86ae3d9b3035bb01dde
Rustam.Home <ka.po.po@yandex.ru>
2024-10-24 09:47:43 +0500

    Добавление поля API_TOKEN в config

--- core/config.py
+++ core/config.py
@@ -7,18 +7,19 @@ class Settings:
 
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-    DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
     JWT_EXPIRATION_TIME = timedelta(minutes=480)
 
-    REDIS_SERVER = os.getenv("REDIS_SERVER")
+    REDIS_HOST = os.getenv("REDIS_HOST")
     REDIS_PORT = os.getenv("REDIS_PORT")
-    REDIS_URL = f'redis://{REDIS_SERVER}:{REDIS_PORT}'
+    REDIS_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}'
 
+    API_TOKEN = os.getenv("API_TOKEN")
 
 settings = Settings()
